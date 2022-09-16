52abe8c4b4506b75c97e9a9aa93bdb126c5312e5
Camila Santana <cssantana8@gmail.com>
2022-09-16 15:17:00 -0600

    script

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+
+let oLastModif = new Date(document.lastModified);
+// console.log(oLastModif.get);
+
+const month = oLastModif.getMonth() + 1;
+const hours = oLastModif.getHours();
+const minutes = oLastModif.getMinutes();
+const seconds = oLastModif.getSeconds();
+
+const currentDate = month + "/" + oLastModif.getDate() + "/" + oLastModif.getFullYear() + " " + hours + ":" + minutes + ":" + seconds;
+document.getElementById('updated').textContent = currentDate;
